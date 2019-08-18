9eabe9dad7bacc5db4ef4baf4fbbe267ebd977d7
genshijin <vodka041619893616@outlook.jp>
2019-08-19 00:50:54 +0900

    部分テンプレート表示設定、タイトル設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title
+    title = "メルカリ"
+    if @page_title
+      title = title + " - " + @page_title
+    else
+      title = title + " スマホでかんたん フリマアプリ"
+    end
+    title
+  end
 end
