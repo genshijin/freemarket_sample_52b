e374f03cb6f7be95f17b2506c97180045d545a80
genshijin <vodka041619893616@outlook.jp>
2019-08-24 11:45:09 +0900

    fix to page_title method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
   def page_title
-    title = "メルカリ"
     if @page_title
-      title = title + " - " + @page_title
+      title = "メルカリ" + " - " + @page_title
     else
-      title = title + " スマホでかんたん フリマアプリ"
+      title = "メルカリ" + " スマホでかんたん フリマアプリ"
     end
     title
   end
