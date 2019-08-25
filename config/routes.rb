175bada694763540a3dcc7b8f5aaebcbc38b913f
tech-nisshi <bochibochiicoca012@gmail.com>
2019-08-25 13:56:51 +0900

    set path and a_tag change link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  'exhibit#index'
-  # root 'items#index'
+  root 'items#index'
+    resources :exhibit, only: [:index]
+    resources :purchase, only: [:index]
 end
