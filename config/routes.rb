8d894bdd36d5d917dde79a482eb0b4df1e468253
tech-nisshi <bochibochiicoca012@gmail.com>
2019-09-11 23:22:11 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
+  resources :items, only: [:index, :show]
   resources :user_profiles, only: [:index]
   resources :exhibit, only: [:index]
   resources :purchase, only: [:index]
