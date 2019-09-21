a52fe044edf4f21537eb6e3e02e57ffe2fad4c3d
SyutoHidano <hidagyu0307@gmail.com>
2019-09-21 22:50:01 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,12 +22,14 @@ set :default_env, {
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
+set :linked_files, %w{ config/secrets.yml }
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-  
+
   desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
