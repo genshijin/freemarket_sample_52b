e409f15cf4f95658deac57ca8b2f7d1d947f1e10
tech-nisshi <bochibochiicoca012@gmail.com>
2019-09-28 16:27:47 +0900

    add_itdex.html.haml

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
+
   def index
-    @item = ITEM.all
+    @items= Item.order("id DESC")
+    # @items.each do |item|
+    #   @images = item.images
+      # @item = item
+      # @item_id = item.id
+      # @image = Image.find_by(item_id: @item_id)
+    # end
+   
   end
 
   def show
+    @item = Item.find(params[:id])
+    @images = @item.images
+    # @seller_name = @item.users
+    
   end
+  
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_21_074054) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -92,9 +91,6 @@ ActiveRecord::Schema.define(version: 2019_09_21_074054) do
     t.integer "postage_burden", null: false
     t.string "shipping_date", null: false
     t.string "shipping_origin_area", null: false
-    t.integer "buyer_id"
-    t.integer "seller_id", null: false
-    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "buyer_id"
