027b522d77f3cb6d7890956b73308ae622e33b31
山守真一朗 <shinichiro0917@icloud.com>
2019-10-07 01:16:31 +0900

    画像投稿機能の修正
    画像を投稿する際にエラーが発生するのでその修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User", optional: true
   belongs_to :seller, class_name: "User",optional: true
   belongs_to :category, optional: true
-  has_many :images
+  # has_many :images
   # accepts_nested_attributes_for :images
   mount_uploader :image, ImageUploader
 
