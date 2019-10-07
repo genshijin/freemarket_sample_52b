12495c6cbeb7e9a740e11c4d9628450badec430e
山守真一朗 <shinichiro0917@icloud.com>
2019-10-07 23:57:10 +0900

    Update exhibit_controller.rb
    controllerにimage_urlとseller.idをとる記述を記載

--- app/controllers/exhibit_controller.rb
+++ app/controllers/exhibit_controller.rb
@@ -1,5 +1,7 @@
 class ExhibitController < ApplicationController
   def show
     @item = Item.find(params[:id])
+    @images = @item.image
+    @seller = User.where('id = ?', @item.seller_id)
   end
 end
