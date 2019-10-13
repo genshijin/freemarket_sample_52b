ec4ab68a6d6e553ff3ccd666e5b182d4b644fe07
SyutoHidano <hidagyu0307@gmail.com>
2019-10-13 20:38:38 +0900

    aaa

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,33 +1,22 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
 
-    function add_html(inputs){
+    function add_html(blobUrl){
       var HTML = `
-      <div class='sellitem_contener'>
-        <div class='sellitem_contener_image'>
-          <img class="edit_image"></img>
-        </div>
-        <div class='sell-image-upload'>
-          <a class='sell-image-upload-btn'>編集</a>
-          <a class='sell-image-upload-btn'>削除</a>
-        </div>
+      <div class='sellitem_contener_image'>
+        <img class="edit_image" src='${blobUrl}' id='image_tag'></img>
       </div>
-      <label class='file_upload_box'>
-      <input style="display: none;" class="sell-upload-drop-file" id="file_input" type="file" name="item[image]">
-        <div class='sell-upload-drop-box have-item-0 edit_page'>
-          <pre class="visible-pc">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード</pre> 
-          <div class='icon-camera'>
-            <i class='fa fa-camera' "aria-hidden"= "true"></i>
-          </div>
-        </div>
-      </label>  `
-      $('#edit_page').append(HTML);
+      <div class='sell-image-upload'>
+        <a class='sell-image-upload-btn'>編集</a>
+        <a class='sell-image-upload-btn'>削除</a>
+      </div>`
+      $('#img').append(HTML);
       
     }
 
 
     $('.input-price').on('keyup', function(e){
-    
+      
       $('#01').empty();
       $('#09').empty();
       var input = $('.input-price').val();
@@ -40,10 +29,10 @@ $(document).on('turbolinks:load', function() {
     })
 
     $('#file_input').on('change', function(e){
-      console.log(inputs);
-      $('#edit_page').empty();
-      var inputs = $('#file_input').val();
-      add_html(inputs);
+      var file = e.target.files[0];
+      var blobUrl = window.URL.createObjectURL(file);
+      $('#img').empty();
+      add_html(blobUrl);
     })
 
   });
