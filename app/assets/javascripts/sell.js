0338b27ed3afa0378ceffd2ac71a834d60fab871
SyutoHidano <hidagyu0307@gmail.com>
2019-10-13 19:22:03 +0900

    aaa

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,8 +1,27 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
 
-    function real_price(price){
-      var HTML = ``
+    function add_html(inputs){
+      var HTML = `
+      <div class='sellitem_contener'>
+        <div class='sellitem_contener_image'>
+          <img class="edit_image"></img>
+        </div>
+        <div class='sell-image-upload'>
+          <a class='sell-image-upload-btn'>編集</a>
+          <a class='sell-image-upload-btn'>削除</a>
+        </div>
+      </div>
+      <label class='file_upload_box'>
+      <input style="display: none;" class="sell-upload-drop-file" id="file_input" type="file" name="item[image]">
+        <div class='sell-upload-drop-box have-item-0 edit_page'>
+          <pre class="visible-pc">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード</pre> 
+          <div class='icon-camera'>
+            <i class='fa fa-camera' "aria-hidden"= "true"></i>
+          </div>
+        </div>
+      </label>  `
+      $('#edit_page').append(HTML);
       
     }
 
@@ -19,5 +38,13 @@ $(document).on('turbolinks:load', function() {
         $('#09').text('¥' + input_09);
       }
     })
+
+    $('#file_input').on('change', function(e){
+      console.log(inputs);
+      $('#edit_page').empty();
+      var inputs = $('#file_input').val();
+      add_html(inputs);
+    })
+
   });
 });
