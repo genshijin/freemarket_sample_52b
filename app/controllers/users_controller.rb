2822366a0cc4d5e12220514a19607ae448c08ffe
山守真一朗 <shinichiro0917@icloud.com>
2019-10-14 00:55:48 +0900

    update my item info page
    マイページ内にあるタブ内を表示させる為の記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @item=Item.where(buyer:current_user[:id])
   end
 
   def edit
@@ -9,12 +10,14 @@ class UsersController < ApplicationController
   end
 
   def exhibition
-    @items=Item.where(seller:current_user[:id])
+    @items=Item.where(seller:current_user[:id],item_status:0)
   end
 
   def trading
+    @items=Item.where(seller:current_user[:id],item_status:1)
   end
 
   def completed
+    @items=Item.where(seller_id:current_user[:id],item_status:2)
   end
 end
