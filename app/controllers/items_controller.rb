5bbee50ee12320fd62802e188e5e691b7b8b2654
genshijin <vodka041619893616@outlook.jp>
2019-10-19 14:10:52 +0900

    ログイン制限、例外の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :logout_rollback, except: [:index, :show]
+  before_action :logout_rollback, except: [:index, :show, :search]
   before_action :set_item, only: [:show, :update, :edit, :destroy]
   before_action :set_search
 
